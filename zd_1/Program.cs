eb71a31819611e3e21206b919d222f0e3f401761
Maria Chernomazova <chernomazova@mari.bk>
2023-07-17 02:28:51 +0500

    Создали программу к задаче

---
+++ zd_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.WriteLine("Введите пятизначное число");
+string a=Console.ReadLine();
+bool is_palyndrome = true;
+for(int i=0;i<a.Length; i++){
+    if (a[i] != a[a.Length-1-i]){
+        is_palyndrome = false;
+    }
+}
+if (is_palyndrome == true) {
+Console.WriteLine("Число является палиндромом");
+} else
+{
+    Console.WriteLine("Число не является палиндромом");
+};
---
+++ zd_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+Console.WriteLine("Введите координаты первой точки - a");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки - в");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+double distance ;
+distance=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)); {
+    Console.WriteLine ("{0} Расстояние между двумя точками в пространстве", distance);
+}
