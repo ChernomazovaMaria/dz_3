a5a23a8f6c95e17b57894f0f5919e260a4ce683d
Maria Chernomazova <chernomazova@mari.bk>
2023-07-18 00:49:46 +0500

    Создали программу

--- zd_2/Program.cs
+++ zd_2/Program.cs
@@ -7,7 +7,5 @@ Console.WriteLine("Введите координаты второй точки -
 double x2 = Convert.ToDouble(Console.ReadLine());
 double y2 = Convert.ToDouble(Console.ReadLine());
 double z2 = Convert.ToDouble(Console.ReadLine());
-double distance ;
-distance=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)); {
-    Console.WriteLine ("{0} Расстояние между двумя точками в пространстве", distance);
-}
+double distance = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)); 
+Console.WriteLine ("{0} Расстояние между двумя точками в пространстве", distance);
---
+++ zd_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число и выдает таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число N");
+int n= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица кубов данного числа");
+for (int i=1; i<=n; i++) {
+    double K= Math.Pow(i,3);
+    Console.WriteLine("{0}", K);
+}
